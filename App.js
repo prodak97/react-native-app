83081ebdaf0fa4707d63545dc28ed752aee3efc8
prodak <prodak97@gmail.com>
2022-07-05 01:15:42 +0400

    expo-image-picker

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image, Platform } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(CameraType.back);
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspects:[4,3],
+      quality:1,
+    });
+    
+    console.log(result);
+    if(!result.cancelled){
+      setImage(result.uri);
+    }
+  }
 
   useEffect(() => {
     (async () => {
@@ -29,6 +45,8 @@ export default function App() {
               setType(type === CameraType.back ? CameraType.front : CameraType.back);
             }}>
             <Text style={styles.text}> Whoa Camera is working! </Text>
+            <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
           </TouchableOpacity>
         </View>
       </Camera>
